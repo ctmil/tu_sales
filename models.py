56aa5b74a78d3b83d82edd958f1d2b68bfafa466
morrillo <gustavo.orrillo@gmail.com>
2017-07-17 11:53:31 -0300

    Added cancellation automation

---
+++ models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from openerp import tools, models, fields, api, _
+from openerp.osv import osv
+from openerp.exceptions import except_orm, ValidationError
+from StringIO import StringIO
+import urllib2, httplib, urlparse, gzip, requests, json
+import openerp.addons.decimal_precision as dp
+import logging
+import datetime
+import time
+from openerp.fields import Date as newdate
+from datetime import datetime,date,timedelta
+
+class sale_order(models.Model):
+        _inherit = 'sale.order'
+
+	@api.model
+	def clean_sale_orders(self):
+		now_2hours = datetime.today() - timedelta(hours=2)
+		orders = self.search([('state','in',['draft','sent'])])
+		for order in orders:
+			if order.state in ['draft','sent']:
+				if order.write_date < str(now_2hours):
+					order.action_cancel()
+				
